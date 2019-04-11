b429437cb4aa51ce6258602b2534327830e3600f
bagaking <zfaustk@qq.com>
2019-04-12 02:19:38 +0800

    doc: lock procedure

--- src/api/controllers/lock.ts
+++ src/api/controllers/lock.ts
@@ -1,7 +1,42 @@
+/**
+ * author: bagaking(kinghand@foxmail.com)
+ * date: 2019-04-11T18:03:52.522Z
+ *
+ * roles:
+ *
+ * - RM: single service, ex. game server, trade service, cultivate service
+ * - TM: /lock service
+ *
+ * lock procedure:
+ *
+ *  RM --> |TM/lock/vote(nft_id)| TM (TM prepared)
+ *  TM .-> |return: prepared lock state| RM
+ *
+ *  1. all committed
+ *      1. RM .-> |RM/prepare: success| RM
+ *      2. RM --> |TM/lock/continue(lock_id)| TM (TM committed)
+ *      3. TM .-> |return: committed lock state| RM
+ *      4. RM .-> |RM/commit| RM
+ *
+ *  2. abort
+ *      1. RM .-> |RM/prepare: failed| RM
+ *      2. RM --> |TM/lock/abort(lock_id)| TM (TM committed)
+ *      3. TM .-> |return: aborted lock state| RM
+ *      3. RM .-> |RM/cancel| RM
+ *
+ *  - exception:
+ *      - timeout at 1.2 or 2.2
+ *          1. TM .-> |timeout| TM
+ *      - timeout at 1.3
+ *          1. RM --> |RM/check(lock_id)| TM
+ *          2.1. TM .-> |return: committed lock state| RM ==> origin 1.3
+ *          2.2. TM .-> |return: aborted lock state| RM ==> origin 2.3
+ *
+ */
+
 import {API} from "../decorators";
 import {Authorized, Body, Ctx, CurrentUser, Get, Param, Post} from "routing-controllers";
 import {Context} from "koa";
-import {AdminService} from "../../logic/admin";
 import {LockService} from "../../logic/lock";
 
 @API("/lock")
