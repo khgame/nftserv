31bc6d8e663f178eadbd1b28286d6a7eebd43bc1
bagaking <zfaustk@qq.com>
2019-04-13 15:15:40 +0800

    test (nft): tests for issue, update and some edge case of them

--- test/core.test.ts
+++ test/core.test.ts
@@ -2,7 +2,7 @@ import {assert} from "chai";
 import * as Path from "path";
 import {Global} from "../src/global";
 
-import {spawn, exec, ChildProcessWithoutNullStreams, ChildProcess} from 'child_process'
+import {spawn, exec, ChildProcessWithoutNullStreams, ChildProcess} from 'child_process';
 
 import {createReq} from './createReq';
 import {initServices} from "../src/logic/service";
@@ -11,32 +11,51 @@ import {OpCode} from "../src/logic/model";
 import {forMs} from "kht";
 
 
+/**
+ * prepare data
+ */
+
 const owner_id = `${Math.random()}`;
+let requestBolb = {
+    op_id: `${new ObjectId()}`,
+    owner_id,
+    data: {"test": 1},
+    logic_mark: "hero"
+};
+let updateData = {
+    op_id: `${new ObjectId()}`,
+    nft_id: "",
+    data: {
+        name: "poko",
+        level: 3,
+        category: "cat"
+    }
+};
+
+
 describe(`validate owner_id ${owner_id}`, async function () {
     process.env.NODE_ENV = "production";
     Global.setConf(Path.resolve(__dirname, `../src/conf.default.json`), false);
     // await ();
-    let loginSvr : ChildProcess;
+    let loginSvr: ChildProcess;
     before(async () => {
         await initServices();
     });
 
     beforeEach(async () => {
-        this.timeout(5000);
+        this.timeout(2000);
         loginSvr = exec("npx kh-loginsvr start -m");
-        console.log("=> start login server mock");
+        // console.log("=> start login server mock");
         await forMs(1000);
-        console.log("=> start test");
-    })
+        // console.log("=> start test");
+    });
 
     afterEach(async () => {
-        this.timeout(5000);
         await loginSvr.kill();
-        console.log("=> end login server mock");
-    })
+        // console.log("=> end login server mock");
+    });
 
-    it('init empty', function (done) {
-        this.timeout(10000);
+    it('/v1/nft/list : init empty', function (done) {
         createReq().get(`/v1/nft/list/${owner_id}`)
             .set('Accept', 'application/json')
             .send({})
@@ -52,26 +71,33 @@ describe(`validate owner_id ${owner_id}`, async function () {
     });
 
 
-    it('issue without authorization', function (done) {
-        this.timeout(10000);
+    it('/v1/nft/issue : issue without authorization', function (done) {
+        createReq().post(`/v1/nft/issue`)
+            .set('Accept', 'application/json')
+            .send({
+                ... requestBolb,
+                op_id: `${Math.random()}`
+            })
+            .expect('Content-Type', /json/)
+            .expect(403).end(done); // forbidden
+    });
+
+    it('/v1/nft/issue : issue with wrong authorization', function (done) {
         createReq().post(`/v1/nft/issue`)
             .set('Accept', 'application/json')
             .set('server_id', `#`)
             .send({
-                op_id: `${Math.random()}`,
-                owner_id,
-                data: {test: 1},
-                logic_mark: "hero"
+                ... requestBolb,
+                op_id: `${Math.random()}`
             })
             .expect('Content-Type', /json/)
             .expect(403).end(done);
     });
 
-    it('issue with wrong op_id', function (done) {
-        this.timeout(10000);
+    it('/v1/nft/issue : issue with wrong op_id', function (done) {
         createReq().post(`/v1/nft/issue`)
             .set('Accept', 'application/json')
-            .set('server_id', `mock-server-identity`)
+            .set('server_id', 'mock-server-identity')
             .send({
                 op_id: `${Math.random()}`,
                 owner_id,
@@ -83,19 +109,12 @@ describe(`validate owner_id ${owner_id}`, async function () {
             .end(done);
     });
 
-    it('issue', function (done) {
-        this.timeout(10000);
-        let data = {
-            op_id: `${new ObjectId()}`,
-            owner_id,
-            data: {"test": 1},
-            logic_mark: "hero"
-        };
+    it('/v1/nft/issue : satisfied nft ', function (done) {
         // console.log("data", data);
         createReq().post(`/v1/nft/issue`)
             .set('Accept', 'application/json')
             .set('server_id', `mock-server-identity`)
-            .send(data)
+            .send(requestBolb)
             // .expect('Content-Type', /json/)
             .expect(200)
             .end(function (err, res) {
@@ -105,10 +124,138 @@ describe(`validate owner_id ${owner_id}`, async function () {
                 let result = res.body.result;
                 // console.log("result", result);
                 assert.equal(result.new, true);
-                assert.equal(result.op._id, data.op_id);
+                assert.equal(result.op._id, requestBolb.op_id);
                 assert.equal(result.op.op_code, OpCode.ISSUE);
-                assert.equal(result.op.params.owner_id, data.owner_id);
+                assert.equal(result.op.params.owner_id, requestBolb.owner_id);
+                assert.equal(result.op.params.logic_mark, requestBolb.logic_mark);
+                assert.deepEqual(result.op.params.data, requestBolb.data);
+                updateData.nft_id = result.op.nft_id;
                 done();
             });
     });
+
+    it('/v1/nft/issue : satisfied nft with same id ', function (done) {
+        // console.log("data", data);
+        createReq().post(`/v1/nft/issue`)
+            .set('Accept', 'application/json')
+            .set('server_id', `mock-server-identity`)
+            .send(requestBolb)
+            // .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                let result = res.body.result;
+                // console.log("result", result);
+                assert.equal(result.new, false);
+                assert.equal(result.op.creator, 'mock-server-identity');
+                assert.equal(result.op._id, requestBolb.op_id);
+                assert.equal(result.op.op_code, OpCode.ISSUE);
+                assert.equal(result.op.params.owner_id, requestBolb.owner_id);
+                assert.equal(result.op.params.logic_mark, requestBolb.logic_mark);
+                assert.deepEqual(result.op.params.data, requestBolb.data);
+                updateData.nft_id = result.op.nft_id;
+                done();
+            });
+    });
+
+    it('/v1/nft/list : acquire list', function (done) {
+        createReq().get(`/v1/nft/list/${owner_id}`)
+            .set('Accept', 'application/json')
+            .send({})
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                // console.log(res.body.result);
+                assert.equal(res.body.result.length, 1);
+                assert.equal(res.body.result[0]._id, updateData.nft_id);
+                assert.equal(res.body.result[0].owner_id, requestBolb.owner_id);
+                assert.equal(res.body.result[0].logic_mark, requestBolb.logic_mark);
+                assert.deepEqual(res.body.result[0].data, requestBolb.data);
+                done();
+            });
+    });
+
+    it('/v1/nft/get : acquire nft', function (done) {
+        createReq().get(`/v1/nft/get/${updateData.nft_id}`)
+            .set('Accept', 'application/json')
+            .send({})
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                // console.log(res.body.result);
+                assert.equal(res.body.result._id, updateData.nft_id);
+                assert.equal(res.body.result.owner_id, requestBolb.owner_id);
+                assert.equal(res.body.result.logic_mark, requestBolb.logic_mark);
+                assert.deepEqual(res.body.result.data, requestBolb.data);
+                done();
+            });
+    });
+
+    it('/v1/nft/update : update nft without authorization', function (done) {
+        createReq().get(`/v1/nft/update`)
+            .set('Accept', 'application/json')
+            .send(updateData)
+            .expect(405)
+            .end(done);
+    });
+
+    it('/v1/nft/update : update nft with wrong authorization', function (done) {
+        createReq().get(`/v1/nft/update`)
+            .set('Accept', 'application/json')
+            .send(updateData)
+            .set('server_id', `#`)
+            .expect(405) // method not allowed
+            .end(done);
+    });
+
+    it('/v1/nft/update : update nft with correct authorization', function (done) {
+        createReq().post(`/v1/nft/update`)
+            .set('Accept', 'application/json')
+            .set('server_id', `mock-server-identity`)
+            .send(updateData)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                // console.log(res.body.result);
+                assert.equal(res.body.result.new, true);
+                assert.equal(res.body.result.op.creator, 'mock-server-identity');
+                assert.equal(res.body.result.op._id, updateData.op_id);
+                assert.equal(res.body.result.op.nft_id, updateData.nft_id);
+                assert.deepEqual(res.body.result.op.params.data, updateData.data);
+                done();
+            });
+    });
+
+    it('/v1/nft/update : update nft with same id', function (done) {
+        createReq().post(`/v1/nft/update`)
+            .set('Accept', 'application/json')
+            .set('server_id', `mock-server-identity`)
+            .send(updateData)
+            // .expect('Content-Type', /json/)
+            // .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                console.log(res);
+                assert.equal(res.body.result.new, false);
+                assert.equal(res.body.result.op.creator, 'mock-server-identity');
+                assert.equal(res.body.result.op._id, updateData.op_id);
+                assert.equal(res.body.result.op.nft_id, updateData.nft_id);
+                assert.deepEqual(res.body.result.op.params.data, updateData.data);
+                done();
+            });
+    });
+
 });
