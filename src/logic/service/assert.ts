5da27068dc2eb0b9d44ff2e80a7d05fb0d39d2f3
bagaking <zfaustk@qq.com>
2019-04-14 22:41:13 +0800

    feat: new Assert (with prefix and log)

--- src/logic/service/assert.ts
+++ src/logic/service/assert.ts
@@ -1,24 +1,49 @@
+import {Logger} from "winston";
+import {genLogger} from "./logger";
+
 type StringMethod = () => string;
 
-export function ok<T>(condition: T, msg: string | Error | StringMethod) {
-    if (!condition) {
-        if (msg as StringMethod) {
-            throw new Error((msg as StringMethod)());
-        } else if (msg as string) {
-            throw new Error(msg as string);
-        } else {
-            throw msg as Error;
+export class Assert {
+
+    public log: Logger;
+
+    constructor(public prefix: string) {
+        this.log = genLogger(prefix);
+    }
+
+    ok<T>(condition: T, msg: string | Error | StringMethod) {
+        if (condition) {
+            return;
         }
+        let msgStr: string = '';
+        if (msg as StringMethod) {
+            msgStr = (msg as StringMethod)();
+            msg = new Error(msgStr);
+        } else if (msg as string) {
+            msgStr = msg as string;
+            msg = new Error(msgStr);
+        } else {
+            msgStr = (msg as Error).message;
+        }
+        this.log.error(msgStr);
+        throw msg;
+    }
+
+    sEqual<T>(a: T, b: T, msg: string | Error | StringMethod) {
+        return this.ok(a === b, msg);
+    }
+
+    sNotEqual<T>(a: T, b: T, msg: string | Error | StringMethod) {
+        return this.ok(a !== b, msg);
     }
 }
 
-export function sEqual<T>(a: T, b: T, msg: string | Error | StringMethod) {
-    return ok(a === b, msg);
+export function genAssert(prefix: string) {
+    return new Assert(prefix);
 }
 
-export function sNotEqual<T>(a: T, b: T, msg: string | Error | StringMethod) {
-    return ok(a !== b, msg);
-}
+
+
 
 
 
