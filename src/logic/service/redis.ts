e6a58685d41722cfd2de332a0e4863d923b324d6
bagaking <zfaustk@qq.com>
2019-04-15 00:09:07 +0800

    feat (redis): redis lock wait for result

--- src/logic/service/redis.ts
+++ src/logic/service/redis.ts
@@ -1,8 +1,10 @@
 import {Global} from "../../global";
 import * as IORedis from "ioredis";
+import {forMs} from "kht/lib";
 
 let redisInstance: IORedis.Redis;
 export const redis = () => {
+    redisLockCountMax = Global.conf.redis.key_mutex_wait_threshold || 100;
     return redisInstance || (redisInstance = new IORedis(Global.conf.redis));
 };
 
@@ -10,13 +12,25 @@ export const getRedisKey =
     (...keys: string[]) =>
         keys.reduce((_, s) => `${_}:${s}`);
 
+let redisLockCountMax = 100;
+let redisLockCount: any = {};
+
 export const redisLock =
     async <T>(key: string,
               lockerIdentity: string,
               bWaitForLock: boolean = true,
               lockTime: number = 5000) => {
+        redisLockCount[key] = redisLockCount[key] ? redisLockCount[key] + 1 : 0;
+        if (redisLockCount[key] >= 100) {
+            throw new Error(`redisLock error : too much(${redisLockCount[key]}) wait for key<${key}>`);
+        }
         const redisKey = getRedisKey("mutex", key);
-        return 'OK' === await Promise.resolve(redis().send_command("set", redisKey, lockerIdentity, "PX", lockTime, "NX"));
+        let ret: any;
+        do {
+            ret = await Promise.resolve(redis().send_command("set", redisKey, lockerIdentity, "PX", lockTime, "NX"));
+        } while (ret !== "OK" || !bWaitForLock || (await forMs(5 * redisLockCount[key]) && false));
+        redisLockCount[key] -= 1;
+        return 'OK' === ret;
     };
 
 export const redisUnlock =
@@ -24,7 +38,7 @@ export const redisUnlock =
               lockerIdentity: string) => {
         const redisKey = getRedisKey("mutex", key);
         if (lockerIdentity !== await redis().get(redisKey)) {
-            return ;
+            return;
             // throw new Error("redis unlock error: locker identity are not match"); // don't interrupt the program
         }
         await redis().pexpire(redisKey, 1);
