654f5ffc7f08f36756d2a0cbddbb01e23a3b8815
bagaking <zfaustk@qq.com>
2019-04-19 23:10:31 +0800

    test: add method itPost and itGet to createReq.ts

--- test/createReq.ts
+++ test/createReq.ts
@@ -1,7 +1,79 @@
 import {ApiApplication} from "../src/api";
 import * as request from "supertest";
+import {Test} from "supertest";
+import chalk from "chalk";
 
 export function createReq() {
     const server = (new ApiApplication()).server;
     return request(server);
+}
+
+export function itPost(desc: string,
+                       url: string | (() => string),
+                       header: any = {},
+                       data: any = {},
+                       expect: (req: Test) => any,
+                       validate?: (body: any) => any) {
+    it(desc + " : POST[" + url + "]", done => {
+        const urlFinal = typeof url === 'string' ? url : url();
+        if (typeof url !== 'string') {
+            console.log(chalk.gray("\t■ POST url :"), chalk.blue(urlFinal));
+        }
+        let req = createReq().post(urlFinal).set('Accept', 'application/json');
+        for (const key in header) {
+            if (!header.hasOwnProperty(key)) {
+                continue;
+            }
+            req = req.set(key, header[key]);
+        }
+        req = req.send(data);
+        expect(req);
+        req.end(function (err, res) {
+            if (err) {
+                console.log("------error------", desc, "\n[data] :\n", data, "\n[res.body] :\n", res.body, "\n-----------------");
+                return done(err);
+            }
+            const result = res.body;
+            if (validate) {
+                validate(result);
+            }
+            done();
+        });
+        return;
+    });
+}
+
+
+export function itGet(desc: string,
+                      url: string | (() => string),
+                      header: any = {},
+                      expect: (req: Test) => any,
+                      validate?: (body: any) => any) {
+    it(`${desc} : GET[${url}]`, done => {
+        const urlFinal = typeof url === 'string' ? url : url();
+        if (typeof url !== 'string') {
+            console.log(chalk.gray("\t◆ GET url :"), chalk.blue(urlFinal));
+        }
+        let req = createReq().get(urlFinal).set('Accept', 'application/json');
+        for (const key in header) {
+            if (!header.hasOwnProperty(key)) {
+                continue;
+            }
+            req = req.set(key, header[key]);
+        }
+        req = req.send({});
+        expect(req);
+        req.end(function (err, res) {
+            if (err) {
+                console.log(desc, "res.body:", res.body);
+                return done(err);
+            }
+            const result = res.body;
+            if (validate) {
+                validate(result);
+            }
+            done();
+        });
+        return;
+    });
 }
